359672fd29479399637188c31b1af723c14366ff
Todd <ihatespam@gmail.com>
2024-11-12 18:45:01 -0600

    submenu

--- pirate/src/components/labels/Copyright.vue
+++ pirate/src/components/labels/Copyright.vue
@@ -3,35 +3,53 @@ import Package from "@/../package.json"
 import { inject } from "vue"
 
 const mainState = inject("state") as MainState
-
+const siteRoot = window.location.origin
 const atprotoApiVersion = (Package.dependencies["@atproto/api"] ?? "").replace(/^[\^|~]/, "")
 
 const yyyy = (new Date).getFullYear()
 </script>
 
 <template>
+
+
   <div class="copyright">
-    <small>Klearsky</small>
+    <small>PIRATE</small>
     <RouterLink
       v-if="mainState.atp.hasLogin()"
       class="textlink--underline"
-      :to="{ path: '/search/post', query: { text: 'from:mimonelu.net ⭐ Klearsky | 🔥 Klearsky' } }"
+      :to="{ path: '/search/post', query: { text: 'pirate social' } }"
     >
       <span>v{{ Package.version }}</span>
     </RouterLink>
     <span v-else>v{{ Package.version }}</span>
-    <small>alpha (@atproto/api v{{ atprotoApiVersion }}) &copy; {{ yyyy }} mimonelu</small>
+    <small>alpha (@atproto/api v{{ atprotoApiVersion }}) <a
+      class="textlink--underline"
+      href="https://piratesocial.org"
+      rel="noreferrer"
+      target="_blank"
+    ></a> <a
+    class="textlink--underline"
+    href="../../../admin"
+    rel="noreferrer"
+  >
+    <span>Site Admin</span>
+  </a></small>
+  
+    
+
+    <!-- |
     <a
       class="textlink--underline"
-      href="https://github.com/mimonelu/klearsky"
+      href="https://github.com/piratesocial/pirate"
       rel="noreferrer"
       target="_blank"
     >
       <span>GitHub</span>
-    </a>
+    </a> -->
+    <!-- <br />
     <RouterLink
       class="textlink--underline"
-      :to="{ path: '/profile/feeds', query: { account: 'mimonelu.net' } }"
+      :to="{ path: '/profile/feeds', query: { account: 'ihatespam@gmail.com' } }"
     >
       <span>Bluesky</span>
     </RouterLink>
@@ -44,7 +62,7 @@ const yyyy = (new Date).getFullYear()
       target="_blank"
     >
       <span>Web</span>
-    </a>
+    </a> -->
   </div>
 </template>
 
--- pirate/src/components/shells/SubMenu.vue
+++ pirate/src/components/shells/SubMenu.vue
@@ -73,7 +73,7 @@ function openKeywordHistoryPopover ($event: Event) {
     <MyFeedList />
 
     <!-- コピーライト -->
-  <!--  <CopyRight /> -->
+   <CopyRight />
   </div>
 </template>
 
